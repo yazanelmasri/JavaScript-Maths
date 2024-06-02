7561a576c81d3f6a9290cd5cd1b6cc62ccda16ee
Yazan El Masri <yazan_elmasri@hotmail.com>
2024-06-02 11:21:54 +0000

    Division

--- assets/js/script.js
+++ assets/js/script.js
@@ -139,3 +139,12 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "x";
 
 }
+
+function displayDivisionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "÷";
+
+}
+
